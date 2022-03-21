05f677d6a175c796d88bfac4f71ddf35d5bc314d
IgnacioCurto <ignacio.n.curto@gmail.com>
2022-03-21 10:47:50 -0300

    Added color mode

--- assets/js/colorMode.js
+++ assets/js/colorMode.js
@@ -1,23 +1,23 @@
 const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () => {
-	document.body.classList.toggle('light');
+	document.body.classList.toggle('color');
 	btnSwitch.classList.toggle('active');
 
 	// Guardamos el modo en localstorage.
-	if(document.body.classList.contains('light')){
-		localStorage.setItem('light-mode', 'true');
+	if(document.body.classList.contains('color')){
+		localStorage.setItem('color-mode', 'true');
 	} else {
-		localStorage.setItem('light-mode', 'false');
+		localStorage.setItem('color-mode', 'false');
 	}
 });
 
 // Obtenemos el modo actual.
-if(localStorage.getItem('light-mode') === 'true'){
-	document.body.classList.add('light');
+if(localStorage.getItem('color-mode') === 'true'){
+	document.body.classList.add('color');
 	btnSwitch.classList.add('active');
 } else {
-	document.body.classList.remove('light');
+	document.body.classList.remove('color');
 	btnSwitch.classList.remove('active');
 }
 
