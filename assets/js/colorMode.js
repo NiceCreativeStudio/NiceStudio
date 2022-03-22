abfb8ca1168b55f815e59c5da103767e95ae634e
IgnacioCurto <ignacio.n.curto@gmail.com>
2022-03-22 08:59:59 -0300

    Index layout & color

--- assets/js/colorMode.js
+++ assets/js/colorMode.js
@@ -3,12 +3,15 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
 	document.body.classList.toggle('color');
 	btnSwitch.classList.toggle('active');
-
+	document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/new-moon_1f311.png";
+	
 	// Guardamos el modo en localstorage.
 	if(document.body.classList.contains('color')){
 		localStorage.setItem('color-mode', 'true');
+		document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/new-moon_1f311.png";
 	} else {
 		localStorage.setItem('color-mode', 'false');
+		document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/rainbow_1f308.png";
 	}
 });
 
@@ -19,9 +22,5 @@ if(localStorage.getItem('color-mode') === 'true'){
 } else {
 	document.body.classList.remove('color');
 	btnSwitch.classList.remove('active');
-}
 
-/* 	$("body").css("transition", "0s ease all")
-	$(".nav__box").css("transition", "0s ease all")
-	$("body").css("transition", ".3s ease all")
-	$(".nav__box").css("transition", ".3s ease all") */
+}
