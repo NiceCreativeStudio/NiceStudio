b37b7a32fdd6c535a592bdae09882ae71cedad15
IgnacioCurto <ignacio.n.curto@gmail.com>
2022-03-28 08:58:15 -0300

    Navbar & multiple fixes

--- assets/js/colorMode.js
+++ assets/js/colorMode.js
@@ -3,7 +3,6 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
 	document.body.classList.toggle('color');
 	btnSwitch.classList.toggle('active');
-	document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/new-moon_1f311.png";
 	
 	// Guardamos el modo en localstorage.
 	if(document.body.classList.contains('color')){
@@ -19,8 +18,21 @@ btnSwitch.addEventListener('click', () => {
 if(localStorage.getItem('color-mode') === 'true'){
 	document.body.classList.add('color');
 	btnSwitch.classList.add('active');
+	document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/new-moon_1f311.png";
 } else {
 	document.body.classList.remove('color');
 	btnSwitch.classList.remove('active');
-
+	document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/rainbow_1f308.png";
 }
+
+/* function CheckColor(){
+	if(localStorage.getItem('color-mode') === 'true'){
+		document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/new-moon_1f311.png";
+	}else{
+		document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/rainbow_1f308.png";
+	}
+}
+
+$(body).load(function () {
+	CheckColor(); 
+}); */
