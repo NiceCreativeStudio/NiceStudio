aab22b0a881014428aaa65559ccef84ac3eec918
IgnacioCurto <ignacio.n.curto@gmail.com>
2022-03-29 10:13:03 -0300

    new moon emoji

--- assets/js/colorMode.js
+++ assets/js/colorMode.js
@@ -7,7 +7,7 @@ btnSwitch.addEventListener('click', () => {
 	// Guardamos el modo en localstorage.
 	if(document.body.classList.contains('color')){
 		localStorage.setItem('color-mode', 'true');
-		document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/first-quarter-moon_1f313.png";
+		document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/new-moon-face_1f31a.png";
 	} else {
 		localStorage.setItem('color-mode', 'false');
 		document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/rainbow_1f308.png";
@@ -18,7 +18,7 @@ btnSwitch.addEventListener('click', () => {
 if(localStorage.getItem('color-mode') === 'true'){
 	document.body.classList.add('color');
 	btnSwitch.classList.add('active');
-	document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/first-quarter-moon_1f313.png";
+	document.getElementById("switch").src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/new-moon-face_1f31a.png";
 } else {
 	document.body.classList.remove('color');
 	btnSwitch.classList.remove('active');
