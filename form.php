38b7f6195c7df4da6ba57b3f07030805a77accfe
IgnacioCurto <ignacio.n.curto@gmail.com>
2022-03-31 13:17:09 -0300

    working form

--- form.php
+++ form.php
@@ -1,17 +1,23 @@
 <?php
-    $receiver = 'hello@nicecreativestudio.com';
+    $previous = "javascript:history.go(-1)";
+    
+    if(isset($_POST['SubmitButton'])){ //check if form was submitted
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $description = $_POST['description'];
-    $launch = $_POST['launch__date'];
-    $budget = $_POST['budget'];
-    $hear = $_POST['hear'];
-
-    $header = "Sent from website contact form";
-    $content = "\n Hi! We're " . $email . "\n About our project: " . $description . "\n Our ideal launch date is: " . $launch . "\n Our budget is: " . $budget . "\n We heard about you through " . $hear;
-    mail($receiver, $name, $content, $header);
-    if ($mail) {
-        echo "<h1> Testttt </h1>";
-    }
+        $receiver = 'hellonicestudio@gmail.com';
+    
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $description = $_POST['description'];
+        $launch = $_POST['launch__date'];
+        $budget = $_POST['budget'];
+        $hear = $_POST['hear'];
+    
+        $header = "Sent from website contact form";
+        $content = "\n Hi! We're " . $email . "\n About our project: " . $description . "\n Our ideal launch date is: " . $launch . "\n Our budget is: " . $budget . "\n We heard about you through " . $hear;
+        
+        mail($receiver, $name, $content, $header);
+        
+    } 
+    echo("<script>alert('We´ll be on touch!')</script>");
+    echo("<script>window.location = 'contact.html';</script>");
 ?>
