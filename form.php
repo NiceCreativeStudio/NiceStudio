5c81c01fe6216d47190bc9f557f80fff9fe52d4c
IgnacioCurto <ignacio.n.curto@gmail.com>
2022-03-31 11:20:49 -0300

    Contact form testing

--- form.php
+++ form.php
@@ -4,11 +4,14 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $description = $_POST['description'];
-    $launch = $_POST['launch'];
+    $launch = $_POST['launch__date'];
     $budget = $_POST['budget'];
     $hear = $_POST['hear'];
 
     $header = "Sent from website contact form";
     $content = "\n Hi! We're " . $email . "\n About our project: " . $description . "\n Our ideal launch date is: " . $launch . "\n Our budget is: " . $budget . "\n We heard about you through " . $hear;
     mail($receiver, $name, $content, $header);
+    if ($mail) {
+        echo "<h1> Testttt </h1>";
+    }
 ?>
